4b3192807fc4f8f35bfd1cf8923361b8a8682c2c
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-25 01:36:25 +0100

    variable name error fxed

--- codewars.py
+++ codewars.py
@@ -1,35 +1,38 @@
 # TODO: create the User class
 # it must support rank, progress, and the inc_progress(rank) method
 
-rankings = [x for x in range(-8,9) if x != 0]
+rankings = [x for x in range(-8, 9) if x != 0]
 
-def calculate(user_ranking,activity_ranking):
+
+def calculate(user_ranking, activity_ranking):
     if user_ranking == activity_ranking:
         return 3
-    elif activity_ranking == (user_ranking-1):
+    elif activity_ranking == (user_ranking - 1):
         return 1
     elif activity_ranking > user_ranking:
-        diff = (activity_ranking)-(user_ranking)
-        return (10*diff*diff)
-    elif (user_ranking-activity_ranking) >=2:
+        diff = (activity_ranking) - (user_ranking)
+        return 10 * diff * diff
+    elif (user_ranking - activity_ranking) >= 2:
         return 0
-    
-  
-class User():
-    def __init__(self,rank=-8,progress=0):
+
+
+class User:
+    def __init__(self, rank=-8, progress=0):
         self.rank = rank
         self.progress = progress
+
     def rank(self):
         return rankings[self.rank]
+
     def progress(self):
         return self.progress
-    def inc_progress(self,activity_rating):
-        progress = calculate(rankings[self.rank],activity_ranking)
-        self.progress+=progress
-        while self.progress >=100:
-            if self.rank<len(rankings)-1:
-                self.progress -=100
-                self.rank+=1
-                
+
+    def inc_progress(self, activity_ranking):
+        progress = calculate(rankings[self.rank], activity_ranking)
+        self.progress += progress
+        while self.progress >= 100:
+            if self.rank < len(rankings) - 1:
+                self.progress -= 100
+                self.rank += 1
+
         return 0
-    
