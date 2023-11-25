a99634ade72432a238a7703685e1afb5ed70a0ee
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-25 15:45:56 +0100

    changes

--- codewars.py
+++ codewars.py
@@ -1,19 +1,25 @@
 # TODO: create the User class
 # it must support rank, progress, and the inc_progress(rank) method
 
-rankings = [x for x in range(-8, 9) if x != 0]
+rankings = [int(x) for x in range(-8, 9) if x != 0]
 
 
 def calculate(user_ranking, activity_ranking):
+    print("user_ranking", user_ranking)
+
+    print("activity_ranking", activity_ranking)
     if user_ranking == activity_ranking:
         return 3
     elif activity_ranking == (user_ranking - 1):
         return 1
     elif activity_ranking > user_ranking:
+        if activity_ranking > 0:
+            activity_ranking -= 1
+        if user_ranking > 0:
+            user_ranking -= 1
         diff = (activity_ranking) - (user_ranking)
         return 10 * diff * diff
-    elif (user_ranking - activity_ranking) >= 2:
-        return 0
+    return 0
 
 
 class User:
@@ -21,18 +27,23 @@ class User:
         self.rank = rank
         self.progress = progress
 
-    def rank(self):
-        return rankings[self.rank]
-
-    def progress(self):
-        return self.progress
-
-    def inc_progress(self, activity_ranking):
-        progress = calculate(rankings[self.rank], activity_ranking)
+    def inc_progress(self, activity):
+        progress = calculate(self.rank, activity)
+        #         print ("rankings",rankings[self.rank])
+        #         print("progress",progress)
+        #         print("activity_ranking",activity_rankinge)
         self.progress += progress
         while self.progress >= 100:
-            if self.rank < len(rankings) - 1:
+            print(self.progress)
+            print(self.rank)
+            if self.rank < 9:
                 self.progress -= 100
                 self.rank += 1
+            elif self.rank == -1:
+                self.rank += 2
+                self.progress -= 100
 
-        return 0
+        if self.rank >= 0:
+            self.rank += 1
+
+        print(self.rank)
